b11628827dd9820fa7d14daa94cb6b41e334b7dd
diabl <diabl@DESKTOP-VH9G7RK>
2021-08-11 03:49:29 +0800

    第二次

--- python/B0729015_HW3.py
+++ python/B0729015_HW3.py
@@ -1,5 +1,6 @@
 import csv
 import jieba
+import math
 
 def get_movie_data(name):
     movie_data = dict()
@@ -18,5 +19,62 @@ def jieba_cal(data):
    
     return word
 
-name = "dct.csv"
-get_movie_data(name)    
+def cal_all_word_num(word_list):         #
+    count_list = []
+        
+    for i in range(0,len(word_list)):
+        count_list.append(len(word_list[i]))
+
+    return count_list
+ 
+def tf_algorithm(word_freq_list, count_list): #
+    freq_list = []
+    length = len(word_freq_list)
+    
+    for i in range(0, length):
+        temp = dict()
+        for key, value in word_freq_list[i].items():
+            temp[key] = (value / count_list[i])
+        freq_list.append(temp)
+        
+    return freq_list
+
+
+def idf_algorithm(word_freq_list):  #
+
+    length = len(word_freq_list) #
+    all_word = []
+    ifd_dict = dict()
+    temp = dict()
+    
+    for i in range(0, length):
+        all_word.extend(word_freq_list[i])
+    
+    for word in all_word:
+        if temp.__contains__(word) != True:
+            temp[word] = 1
+        else:
+            temp[word] += 1 
+    
+    for key, value in temp.items():
+        ifd_dict[key] = math.log(length/value,10)
+    
+    return ifd_dict
+
+def tfidf_algorithm(tf_num,idf_num): #
+    tfidf_num = []
+
+    for i in range(0,len(tf_num)):
+        temp = dict()
+        for key,value in tf_num[i].items():
+            temp[key] = value * idf_num[key]
+        tfidf_num.append(temp)
+    
+    return tfidf_num
+
+def main():
+    name = "dct.csv"
+    get_movie_data(name)  
+  
+if __name__== "__main__":
+    main() 
