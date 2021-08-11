91d326c66eeea5689ee77567de05532a44a56bc2
diabl <diabl@DESKTOP-VH9G7RK>
2021-08-11 23:13:19 +0800

    第三次

--- python/B0729015_HW2.py
+++ python/B0729015_HW2.py
@@ -1,14 +1,38 @@
 import requests
 from bs4 import BeautifulSoup
-import time
 import math
+import random
 from fake_useragent import UserAgent
 import csv
 
 def get_soup(address):
     user_agent = UserAgent()
+    proxy_list = [
+        'http://138.68.60.8:8080',
+        'http://209.97.150.167:8080',
+        'http://191.96.42.80:8080',
+        'http://198.199.86.11:3128',
+        'http://35.232.28.68:8080',
+        'http://170.106.175.94:80',
+        'http://52.149.152.236:80',
+    ]
+    
+    proxy_ip = random.choice(proxy_list) # 隨機取得代理ip
 
-    r = requests.get(url = address, headers={ 'user-agent': user_agent.random })
+    headers = {
+        'http': proxy_ip,
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9", 
+        "Accept-Encoding": "gzip, deflate, br", 
+        "Accept-Language": "zh-TW,zh;q=0.9", 
+        "Host": "movies.yahoo.com.tw",  #目標網站 
+        "Sec-Fetch-Dest": "document", 
+        "Sec-Fetch-Mode": "navigate", 
+        "Sec-Fetch-Site": "none", 
+        "Upgrade-Insecure-Requests": "1", 
+        "User-Agent": user_agent.random
+    }
+    
+    r = requests.get(url = address, headers = headers)
     soup = BeautifulSoup(r.text,"html.parser")
     
     return soup
@@ -17,7 +41,7 @@ def get_movie_address_main():
     url = "https://movies.yahoo.com.tw/moviegenre_result.html?"
     movie_address = []
     
-    for web_id in range(1, 2):
+    for web_id in range(1, 5):
         address_main = url+"genre_id="+str(web_id)
         soup = get_soup(address_main)
         movie_address.extend(get_movie_address(soup))
@@ -26,13 +50,14 @@ def get_movie_address_main():
         local1 = length_str.find("共")
         local2 = length_str.find("筆")
         length = math.ceil(int(length_str[local1+1:local2])/10)
-        #length = 3
-        print(length)
+        
         for page in range(2, length):
             address_mov = address_main + "&page=" + str(page)
             soup = get_soup(address_mov)
             movie_address.extend(get_movie_address(soup))
-    
+            
+        print(web_id)
+        
     return movie_address
     
 def get_movie_address(soup):
@@ -55,7 +80,6 @@ def get_movie_info(address):
   
     for info1 in main_info:
         title = info1.find('h1').text
-        title_en = info1.find('h3').text
         date = info1.findAll('span')[0].text[5:]
         length = info1.findAll('span')[1].text[5:]
         company = info1.findAll('span')[2].text[5:]
@@ -71,29 +95,25 @@ def get_movie_info(address):
         for x in temp:
             tag = x.text.strip()
             break
-    
-    if title_en == " ":
-        title_en = "None"
         
-    data[title] = [title_en, tag, date, length, company, imdb, director, actor, desc]
+    data[title] = [tag, date, length, company, imdb, director, actor, desc]
     return data
     
 def main():
     movie_info = dict()
     address_set = set(get_movie_address_main())
     address_list = list(address_set)
-    
-    for i in range(0, len(address_list)):
-        movie_info = {**movie_info, **get_movie_info(address_list[i])}
-        
-    print(movie_info)    
+          
     with open('dct.csv', 'w', newline='', encoding="utf-8") as file:
         writer = csv.writer(file)
-        writer.writerow(["movie No.", "Name", "Name_en", "Tag", "Date", "Length", "Company", "Imdb", "Director", "Actor", "Desc"])
         j = 1
-        for title,data in movie_info.items():
-            writer.writerow([j, title, data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7], data[8]])
-            j += 1
+        for i in range(0, len(address_list)):
+            movie_info = get_movie_info(address_list[i])
+            print(movie_info)
+            writer.writerow(["movie No.", "Name", "Tag", "Date", "Length", "Company", "Imdb", "Director", "Actor", "Desc"])
+            for title,data in movie_info.items():
+                writer.writerow([j, title, data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7]])
+                j += 1
         
 if __name__== "__main__":
     main() 
--- python/B0729015_HW3.py
+++ python/B0729015_HW3.py
@@ -1,6 +1,8 @@
 import csv
 import jieba
 import math
+import time
+
 
 def get_movie_data(name):
     movie_data = dict()
@@ -9,72 +11,115 @@ def get_movie_data(name):
         for row in rows:
             if row['movie No.'] != " " and row['Desc'] != " ":
                 movie_data[row['movie No.']] = row['Desc']
+                
     return movie_data
 
 def jieba_cal(data):
     word = dict()
-    for key,value in data:
+    for key,value in data.items():
         sentence = jieba.lcut(value, cut_all=False)
         word[key] = sentence
    
     return word
 
-def cal_all_word_num(word_list):         #
-    count_list = []
-        
-    for i in range(0,len(word_list)):
-        count_list.append(len(word_list[i]))
-
-    return count_list
- 
-def tf_algorithm(word_freq_list, count_list): #
-    freq_list = []
-    length = len(word_freq_list)
+def tf_algorithm(word_dict): 
     
-    for i in range(0, length):
+    tf_dict = dict()
+    
+    for number, word_list in word_dict.items():             #word_list為所有電影的字詞list
         temp = dict()
-        for key, value in word_freq_list[i].items():
-            temp[key] = (value / count_list[i])
-        freq_list.append(temp)
+        length = len(word_list)
         
-    return freq_list
+        for i in range(0,length):
+            if word_list[i] not in temp:
+                temp[word_list[i]] = 1
+            elif word_list[i] in temp:
+                temp[word_list[i]] += 1
+        
+        for key,value in temp.items():
+            temp[key] = value / length         #計算TF值
+          
+        tf_dict[number] = temp                 #建立tf的dict
+            
+    return tf_dict
 
-
-def idf_algorithm(word_freq_list):  #
-
-    length = len(word_freq_list) #
-    all_word = []
-    ifd_dict = dict()
-    temp = dict()
+def idf_algorithm(word_dict, tf_dict):  
+    all_word = set()
+    idf_dict = dict()
+    length = len(word_dict)
     
-    for i in range(0, length):
-        all_word.extend(word_freq_list[i])
-    
+    for word_list in word_dict.values():
+        word_set = set(word_list)
+        all_word.update(word_set)
+
     for word in all_word:
-        if temp.__contains__(word) != True:
-            temp[word] = 1
-        else:
-            temp[word] += 1 
-    
-    for key, value in temp.items():
-        ifd_dict[key] = math.log(length/value,10)
-    
-    return ifd_dict
+        idf = 1 #idf=0會有問題
+        
+        for value in tf_dict.values():                  #計算所有存在該word的檔案
+            if word in value:  
+                idf += 1
+        
+        idf_dict[word] = math.log(length / idf, 10)  #某word的idf值
+        
+    return idf_dict
 
-def tfidf_algorithm(tf_num,idf_num): #
-    tfidf_num = []
-
-    for i in range(0,len(tf_num)):
+def tfidf_algorithm(tf_dict, idf_dict): #
+    tfidf_dict = dict()
+    
+    for number, word_dict in tf_dict.items():
+        
         temp = dict()
-        for key,value in tf_num[i].items():
-            temp[key] = value * idf_num[key]
-        tfidf_num.append(temp)
+        
+        for key,value in word_dict.items():     #計算tfidf的值
+            temp[key] = value * idf_dict[key]
+        
+        tfidf_dict[number] = temp
+
+    return tfidf_dict
+
+def calc_distance(point1, point2):      #計算文章與文章的距離
+    distance = 0
     
-    return tfidf_num
+    for key, value in point1.items():
+        if point2.get(key):
+            distance += pow((value - point2[key]), 2)
+        else:
+            distance += pow(value, 2)
+                  
+    return math.sqrt(distance)
+
+def calc_distance_main(tfidf_data):
+    length = len(tfidf_data)
+    distance = []
+    
+    for i in range(1, length):
+        temp = dict()
+        
+        for j in range(1, length):
+            point1 = tfidf_data[str(i)]
+            point2 = tfidf_data[str(j)]
+            
+            temp[str(j)] = calc_distance(point1, point2)
+            
+        distance.append(temp)
+        
+    print(len(distance))
+    return distance
+
+def get_neighbors(distance, k):
+    for i in range(1,len(distance)):
+        distance[i].sort()
+        
 
 def main():
-    name = "dct.csv"
-    get_movie_data(name)  
+    name = "dct1.csv"
+    movie_data = get_movie_data(name)  
+    word_dict = jieba_cal(movie_data)
+    
+    tf = tf_algorithm(word_dict)
+    idf = idf_algorithm(word_dict,tf)
+    tfidf = tfidf_algorithm(tf, idf)
+    distance = calc_distance_main(tfidf)
   
 if __name__== "__main__":
     main() 
