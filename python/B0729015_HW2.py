2e44852d42f10826641de55a1baf2a354ce2d3e2
diabl <diabl@DESKTOP-VH9G7RK>
2021-08-11 23:43:57 +0800

    第四次

--- python/B0729015_HW2.py
+++ python/B0729015_HW2.py
@@ -41,7 +41,7 @@ def get_movie_address_main():
     url = "https://movies.yahoo.com.tw/moviegenre_result.html?"
     movie_address = []
     
-    for web_id in range(1, 5):
+    for web_id in range(1, 6):
         address_main = url+"genre_id="+str(web_id)
         soup = get_soup(address_main)
         movie_address.extend(get_movie_address(soup))
@@ -55,8 +55,6 @@ def get_movie_address_main():
             address_mov = address_main + "&page=" + str(page)
             soup = get_soup(address_mov)
             movie_address.extend(get_movie_address(soup))
-            
-        print(web_id)
         
     return movie_address
     
@@ -79,24 +77,24 @@ def get_movie_info(address):
     data = dict()
   
     for info1 in main_info:
-        title = info1.find('h1').text
-        date = info1.findAll('span')[0].text[5:]
-        length = info1.findAll('span')[1].text[5:]
-        company = info1.findAll('span')[2].text[5:]
-        imdb = info1.findAll('span')[3].text[7:]
-        director = info1.find('div', class_ = 'movie_intro_list').text.strip().replace('\n', '').replace('\r', '').replace(" ", "")
-        actor = info1.findAll('div', class_ = 'movie_intro_list')[1].text.strip().replace(" ", "").replace('\n', '').replace('\r', '')
+        movie_name = info1.find('h1').text.strip()
+        movie_date = info1.findAll('span')[0].text[5:].strip()
+        movie_length = info1.findAll('span')[1].text[5:].strip()
+        movie_company = info1.findAll('span')[2].text[5:].strip()
+        movie_imdb = info1.findAll('span')[3].text[7:].strip()
+        movie_director = info1.find('div', class_ = 'movie_intro_list').text.strip().replace('\n', '').replace('\r', '').replace(" ", "")
+        movie_actor = info1.findAll('div', class_ = 'movie_intro_list')[1].text.strip().replace(" ", "").replace('\n', '').replace('\r', '')
         
     for info2 in desc_info:
-        desc = info2.text.strip().replace('\n', '')
+        movie_desc = info2.text.strip().replace('\n', '')
     
     for info3 in type_info:
         temp = info3.findAll('a', class_ ="gabtn")
         for x in temp:
-            tag = x.text.strip()
+            movie_tag = x.text.strip()
             break
         
-    data[title] = [tag, date, length, company, imdb, director, actor, desc]
+    data[movie_name] = [movie_tag, movie_date, movie_length, movie_company, movie_imdb, movie_director, movie_actor, movie_desc]
     return data
     
 def main():
@@ -106,13 +104,13 @@ def main():
           
     with open('dct.csv', 'w', newline='', encoding="utf-8") as file:
         writer = csv.writer(file)
+        writer.writerow(["movie No.", "Name", "Tag", "Date", "Length", "Company", "Imdb", "Director", "Actor", "Desc"])
         j = 1
         for i in range(0, len(address_list)):
             movie_info = get_movie_info(address_list[i])
-            print(movie_info)
-            writer.writerow(["movie No.", "Name", "Tag", "Date", "Length", "Company", "Imdb", "Director", "Actor", "Desc"])
-            for title,data in movie_info.items():
-                writer.writerow([j, title, data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7]])
+            print(j)
+            for mov_title, mov_data in movie_info.items():
+                writer.writerow([j, mov_title, mov_data[0], mov_data[1], mov_data[2], mov_data[3], mov_data[4], mov_data[5], mov_data[6], mov_data[7]])
                 j += 1
         
 if __name__== "__main__":
